f69de86e216edfe0c01ce8fa6effdac7379ba86e
이정인 <jung020623@g.hongik.ac.kr>
2025-09-06 00:23:28 +0900

    feat : 로그인 페이지 ui 구현

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Layout from './Layout'
 
 import LoginPage from '../pages/login/LoginPage'
+import LoginDetailPage from '../pages/login/LoginDetailPage'
 import HomePage from '../pages/home/HomePage'
 import HomeAsk from '../pages/home/HomeAsk'
 import HomeOffer from '../pages/home/HomeOffer'
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
       { path: 'home/ask', element: <HomeAsk /> },       // /home/ask
       { path: 'home/offer', element: <HomeOffer /> },   // /home/offer
       { path: 'login', element: <LoginPage /> },        // /login
+      { path: 'logindetail', element: <LoginDetailPage /> }, // /logindetail
       { path: 'posts/new', element: <NewPostPage /> },  // /posts/new
       { path: 'posts/:id', element: <PostDetailPage /> }, // /posts/123
       { path: 'my', element: <MyPage /> },              // /my
