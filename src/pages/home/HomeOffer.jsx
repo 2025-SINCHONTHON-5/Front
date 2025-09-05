6fd907876c740dc6b6a74c0e2c1de4d8dc37c2f0
min <lauren@yonsei.ac.kr>
2025-09-05 22:22:08 +0900

    fill the pages' files

---
+++ src/pages/home/HomeOffer.jsx
@@ -0,0 +1,3 @@
+export default function HomeOffer() {
+  return <h1>해드릴게요 페이지</h1>
+}
---
+++ src/pages/my/MyPage.jsx
@@ -0,0 +1,8 @@
+export default function MyPage() {
+  return (
+    <section className="p-6">
+      <h1 className="text-xl font-semibold">마이페이지</h1>
+      <p className="mt-2 text-neutral-600">여기에 내 정보 / 내가 쓴 글 목록이 표시될 예정입니다.</p>
+    </section>
+  )
+}
