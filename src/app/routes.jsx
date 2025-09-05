d02baf5441cd39c61d9086bbe5f374fb660aca35
min <lauren@yonsei.ac.kr>
2025-09-05 21:47:05 +0900

    add app, pages, components folder and files, download react-router-dom, fillmain.jsx, App.jsx, app folder's files

---
+++ src/app/routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+
+import LoginPage from '../pages/login/LoginPage'
+import HomePage from '../pages/home/HomePage'
+import HomeAsk from '../pages/home/HomeAsk'
+import HomeOffer from '../pages/home/HomeOffer'
+import NewPostPage from '../pages/posts/NewPostPage'
+import PostDetailPage from '../pages/posts/PostDetailPage'
+import MyPage from '../pages/my/MyPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },           // /
+      { path: 'home/ask', element: <HomeAsk /> },       // /home/ask
+      { path: 'home/offer', element: <HomeOffer /> },   // /home/offer
+      { path: 'login', element: <LoginPage /> },        // /login
+      { path: 'posts/new', element: <NewPostPage /> },  // /posts/new
+      { path: 'posts/:id', element: <PostDetailPage /> }, // /posts/123
+      { path: 'my', element: <MyPage /> },              // /my
+    ],
+  },
+])
+
+export default router
--- src/context/ThemeContext.jsx
+++
@@ -1,3 +0,0 @@
-const [theme, setTheme] = useState(
-  import.meta.env.VITE_DEFAULT_THEME || Theme.LIGHT
-)
